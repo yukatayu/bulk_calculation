2415c645f26f69ffcac99bcb0583406207d93499
yukatayu <yukatayu.dev@gmail.com>
2019-11-29 18:30:27 +0900

    fix: display

--- main.cpp
+++ main.cpp
@@ -1,15 +1,21 @@
 #include <string>
 #include <iostream>
+#include <iomanip>
 #include <set>
 #include "parse.hpp"
 
 int main(){
 	std::set<std::string> valList;
 	std::string equation;
+	std::cout.setf(std::ios::fixed, std::ios::floatfield);
 	std::cout << "input equation > ";
 	std::cin >> equation;
 	int i = 0;
 	auto parsed = expr(equation, i, valList);
+	if(valList.empty()){
+		std::cout << " >> " << parsed->calc({}) << std::endl;
+		return 0;
+	}
 	for(;;){
 		std::cout << "-----------" << std::endl << std::endl;
 		Context ctx;
